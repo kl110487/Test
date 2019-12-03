bd5f9e7e8d9bbcafca9f7a5fd7b4bcfe3b1b9231
kinga <you@example.com>
2019-12-03 15:28:33 +0100

    gotowe

--- Project1/google.cs
+++ Project1/google.cs
@@ -1,5 +1,6 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -30,43 +31,48 @@ namespace Project1
             Assert.Equal("3", result.Text);
         }
 
-        public void Dispose()
-        {
-            browser.Quit();
-        }
-
-    }
-
-    public class Automatyzacja : IDisposable
-    {
-
-        IWebDriver browser;
-        public Automatyzacja()
-        {
-            browser = new ChromeDriver();
-        }
 
         [Fact]
-        public void Can_Google_From_Warsow()
+        public void Can_add_new_comment()
         {
             browser.Navigate().GoToUrl("http://automatyzacja.benedykt.net/");
             var queryBox = browser.FindElement(By.CssSelector(".entry-title"));
             var link = queryBox.FindElement(By.TagName("a"));
             link.Click();
             var queryBoxComment = browser.FindElement(By.Id("comment"));
-            queryBoxComment.Click();
-            queryBoxComment.SendKeys("test123");
+            var examoleText = Faker.Lorem.Paragraph();
+            queryBoxComment.SendKeys(examoleText);
+            //queryBoxComment.Click();
+            // queryBoxComment.SendKeys("test123");
             var queryBoxAuthor = browser.FindElement(By.Id("author"));
             queryBoxAuthor.SendKeys("bialy");
             var queryBoxEmail = browser.FindElement(By.Id("email"));
             queryBoxEmail.SendKeys("warto@automatyzowac.wp.pl");
             var queryBoxComent = browser.FindElement(By.Id("submit"));
-            queryBoxComent.Click();
+
+            MoveToElement(browser.FindElement(By.CssSelector("div.nav-links")));
+            browser.FindElement(By.Id("submit")).Submit();
+
+            var comments = browser.FindElements(By.CssSelector("article.comment-body"));
+            var myComments = comments
+            .Where(c => c.FindElement(By.CssSelector(".fn")).Text == "bialy")
+            .Where(c => c.FindElement(By.CssSelector(".comment-content > p")).Text == examoleText);
+
+            Assert.Single(myComments);
+        }
+
+        private void MoveToElement(IWebElement element)
+        {
+            Actions builder = new Actions(browser);
+            Actions moveTo = builder.MoveToElement(element);
+            moveTo.Build().Perform();
+
         }
 
         public void Dispose()
         {
             browser.Quit();
         }
+
     }
 }
